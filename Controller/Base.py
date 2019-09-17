6dff922d854d4ea4907d2684bb99f9daab0a7981
dl <948142764@qq.com>
2019-09-17 13:53:31 +0800

    no message

--- Controller/Base.py
+++ Controller/Base.py
@@ -4,3 +4,7 @@ from Controller import app
 @app.route('/hello', methods=['get'])
 def hello():
     return 'Hello World'
+
+@app.route('/hello2', methods=['get'])
+def hello():
+    return 'Hello World2'
