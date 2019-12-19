f043ac421acb4fea7cd78cc2b4b8a5c5f4bab2a5
l d <linboqianxun@gmail.com>
2019-12-19 11:03:11 +0800

    增加访问控制

--- Controller/BaseController.py
+++ Controller/BaseController.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # by dl
 from flask import request
-from Controller import app,cache
+from Controller import app,cache,limiter
 @app.route('/hello', methods=['get'])
-# @cache.cached(timeout=5)
+# @cache.cached(timeout=5) #缓存5s过期
+@limiter.limit('1 per day') #一天一次
 def hello():
     print('没有使用缓存')
     return 'Hello World'
--- Controller/__init__.py
+++ Controller/__init__.py
@@ -3,12 +3,21 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_cache import Cache
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from Config import cache_config
+
 app = Flask(__name__)
 # 缓存
 cache = Cache(app, config=cache_config.Redis, with_jinja2_ext=False)
 cache.init_app(app)
 # 跨域
 CORS(app, supports_credentials=True)
+# 访问控制
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=['200 per day', '50 per hour']
+)
 # 载入控制器
-import Controller.Base
+import Controller.BaseController
